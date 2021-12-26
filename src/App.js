b422a56d99644ea8ed829c01152e860366e2be7a
Rus Hughes <russell.hughes@gmail.com>
2021-12-26 08:15:33 +0100

    web3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      infuraId: "9e4db1dc4a18481ba61bcac2ccdf0827" // required
+    }
+  }
+};
+
+let provider = null;
+let web3 = null;
+let accounts = null;
+
 function App() {
+
+  async function showPortis() {
+      if (!provider) {
+        const web3Modal = new Web3Modal({
+          cacheProvider: true, // optional
+          providerOptions // required
+        });
+        web3 = await connect(web3Modal);
+      }
+
+      //provider._portis.showPortis();
+
+      if (!accounts) {
+        accounts = await web3.eth.getAccounts();
+        console.log(`Wallet address: ${accounts[0].toLowerCase()}`);
+      }
+    }
+
+    async function connect(web3Modal) {
+      provider = await web3Modal.connect();
+      return new Web3(provider);
+    }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Ethereum Test
       </header>
+      <Button type="primary" onClick={() => showPortis()}>
+          Connect
+        </Button>
     </div>
   );
 }
